50432c747cea28f34c2e1a969207490be0c2b78a
Pranavi <inkslinger5198@gmail.com>
2023-09-01 19:38:07 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App.js';
+import './index.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+//This will render the App component (which is the parent component) to the root element in the DOM., this means the application will be hooked to index.html
